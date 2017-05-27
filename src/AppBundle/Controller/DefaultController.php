a6da358654a1b34f45a625eb20c7447e7bf1edf6
Pablo Santiago Semhan <santiagosemhan@gmail.com>
2017-05-27 12:02:27 -0300

    estadisticas

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -8,11 +8,27 @@ use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
-    public function indexAction(Request $request)
+    public function estadisticasAction(Request $request)
     {
         // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
+        return $this->render('AppBundle:Default:estadisticas.html.twig', [
+
+        ]);
+    }
+
+    public function estadisticasHistoricosAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('AppBundle:Default:estadisticas_historicos.html.twig', [
+
+        ]);
+    }
+
+    public function estadisticasTiempoRealAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('AppBundle:Default:estadisticas_tiempo_real.html.twig', [
+
         ]);
     }
 }
